4d6375b1f6424d58d872ae1578176f40fa216850
Luis Gonzalez <lmgl887@gmail.com>
2022-07-12 22:49:59 -0500

    add styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+`
 
 function App() {
 
   return (
-    <h1>From app</h1>
+    <Heading>From app</Heading>
   )
 }
 
