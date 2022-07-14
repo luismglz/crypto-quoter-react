e267ca2ed866d713eb5ec86930b846cea7593851
Luis Gonzalez <lmgl887@gmail.com>
2022-07-13 22:34:46 -0500

    add quote button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,61 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
+import Form from './components/Form';
+import ImageCrypto from './img/image-crypto.png'
+
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width:90%;
+  @media(min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+
+`;
 
 const Heading = styled.h1`
   font-family: 'Lato', sans-serif;
-  color: #fff;
-`
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+  text-transform: uppercase;
+
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+    
+  }
+`;
 
 function App() {
 
   return (
-    <Heading>From app</Heading>
+    <Container>
+      <Image 
+      src={ImageCrypto}
+      alt="Image cryptocurrency logos"
+      />
+      <Heading>Instant cryptocurrency quotes</Heading>
+      <Form/>
+    </Container>
+    
   )
 }
 
